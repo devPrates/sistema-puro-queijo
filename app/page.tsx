eeccd5cfa918ffa4c1d0c9f80096dfebc355f65a
Gabriel Prates Bitencourt <devprates@gmail.com>
2025-02-28 22:02:26 -0400

    chore: project initial setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Puro Queijo</h1>
+      <Button>Teste config</Button>
+    </div>
+  );
+}
