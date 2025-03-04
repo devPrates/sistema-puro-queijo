cb6b4a0b1252b2db5ac057f374b9fbeed3083625
Gabriel Prates Bitencourt <devprates@gmail.com>
2025-03-03 22:24:13 -0400

    feat: Login page updated

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,15 @@
 import { LoginForm } from "./components/login-form";
+import Image from 'next/image';
 
 
 export default function LoginPage() {
   return (
     <div className="h-screen w-full flex items-center justify-center">
       <div className="w-full max-w-sm px-4">
+        <div className="flex justify-center mb-6">
+          <Image src="/logo.png" alt="Logo" width={100} height={100} />
+        </div>
         <div className="text-center mb-8">
-          <h1 className="text-2xl font-bold">Login</h1>
           <p className="text-muted-foreground">Entre com suas credenciais para acessar o sistema</p>
         </div>
         <LoginForm />
